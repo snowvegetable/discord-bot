980ec30501e82205826e46834b74492be5d5a053
佑 宋 <a47462543@gmail.com>
2024-09-15 23:33:09 +0800

    新增ready和messageCreate功能

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-console.log('Hello Discord Bot!');
+import { Client, IntentsBitField } from "discord.js";
+import config from "./config.js";
+
+const TOKEN = config.token;
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", (context) => {
+  console.log(`😃 ${context.user.tag} 已上線`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content === "你好") {
+    message.reply("你好啊！😇");
+  }
+
+  console.log(
+    `id：${message.id}  名稱：${message.author.globalName}  內容：${message.content}`,
+  );
+});
+
+client.login(TOKEN);
